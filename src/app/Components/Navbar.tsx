bd51a6358f766322191ea4dea8f736f5e9e33a28
bisesh shakya <bisesh.shakya@gmail.com>
2024-07-05 01:31:35 -0700

    Fix background, add the navbar social

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client";
+// components/Navbar.tsx
 
 import { useState } from "react";
 import styles from "../styles/Navbar.module.scss";
@@ -7,9 +7,17 @@ import {
   BiUser,
   BiBook,
   BiBriefcase,
-  BiLogoBlogger,
+  BiLogOut,
   BiMailSend,
+  BiMenu,
+  BiX,
 } from "react-icons/bi";
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,49 +29,87 @@ const Navbar: React.FC = () => {
   return (
     <div className={`${styles.container} ${isOpen ? styles.open : ""}`}>
       <div
-        className={`${styles.hamburger} ${isOpen ? styles.open : ""}`}
+        className={`${styles.hamburger} ${
+          isOpen ? styles.open : ""
+        } text-white`}
         onClick={toggleNavbar}
       >
-        <i className={`bi ${isOpen ? "bi-x" : "bi-list"}`}></i>
+        {isOpen ? (
+          <BiX className="bi" size="2em" />
+        ) : (
+          <BiMenu className="bi" size="2em" />
+        )}
       </div>
-      <nav className={`${styles.navbar} ${isOpen ? styles.open : ""}`}>
-        <a href="#" className="me-5">
-          <div className="d-flex justify-content-center">
-            <BiHome className="navbar-icon" size={"1.4em"} />
-          </div>
-          Home
-        </a>
-        <a href="#" className="me-5">
-          <div className="d-flex justify-content-center">
-            <BiUser className="navbar-icon" size={"1.4em"} />
-          </div>
-          About
-        </a>
-        <a href="#" className="me-5">
-          <div className="d-flex justify-content-center">
-            <BiBook className="navbar-icon" size={"1.4em"} />
-          </div>
-          Resume
-        </a>
-        <a href="#" className="me-5">
-          <div className="d-flex justify-content-center">
-            <BiBriefcase className="navbar-icon" size={"1.4em"} />
-          </div>
-          Portfolio
-        </a>
-        <a href="#" className="me-5">
-          <div className="d-flex justify-content-center">
-            <BiLogoBlogger className="navbar-icon" size={"1.4em"} />
-          </div>
-          Blog
-        </a>
-        <a href="#" className="me-5">
-          <div className="d-flex justify-content-center">
-            <BiMailSend className="navbar-icon" size={"1.4em"} />
-          </div>
-          Contact
-        </a>
-      </nav>
+      <div className={`${styles.navbarContainer} ${isOpen ? styles.open : ""}`}>
+        <nav className={styles.navbar}>
+          <a href="#" className="me-5">
+            <div className="d-flex justify-content-center">
+              <BiHome className="navbar-icon" size={"1.4em"} />
+            </div>
+            <strong>
+              <small>HOME</small>
+            </strong>
+          </a>
+          <a href="#" className="me-5">
+            <div className="d-flex justify-content-center">
+              <BiUser className="navbar-icon" size={"1.4em"} />
+            </div>
+            <strong>
+              <small>ABOUT</small>
+            </strong>
+          </a>
+          <a href="#" className="me-5">
+            <div className="d-flex justify-content-center">
+              <BiBook className="navbar-icon" size={"1.4em"} />
+            </div>
+            <strong>
+              <small>RESUME</small>
+            </strong>
+          </a>
+          <a href="#" className="me-5">
+            <div className="d-flex justify-content-center">
+              <BiBriefcase className="navbar-icon" size={"1.4em"} />
+            </div>
+            <strong>
+              <small>PORTFOLIO</small>
+            </strong>
+          </a>
+          <a href="#" className="me-5">
+            <div className="d-flex justify-content-center">
+              <BiLogOut className="navbar-icon" size={"1.4em"} />
+            </div>
+            <strong>
+              <small>BLOG</small>
+            </strong>
+          </a>
+          <a href="#" className="me-5">
+            <div className="d-flex justify-content-center">
+              <BiMailSend className="navbar-icon" size={"1.4em"} />
+            </div>
+            <strong>
+              <small>CONTACT</small>
+            </strong>
+          </a>
+        </nav>
+        <div className={`${styles.socialIcons} ${isOpen ? styles.open : ""}`}>
+          <a href="#">
+            <AiFillGithub className="social-icon" size={"1.4em"} />
+          </a>
+          <a href="#">
+            <AiOutlineTwitter className="social-icon" size={"1.4em"} />
+          </a>
+          <a href="#">
+            <AiFillLinkedin className="social-icon" size={"1.4em"} />
+          </a>
+          <a href="#">
+            <AiOutlineInstagram className="social-icon" size={"1.4em"} />
+          </a>
+        </div>
+        <div className={styles.copyright}>
+          Copyright &#169; 2024 Bisesh Shakya, All rights reserved
+        </div>
+      </div>
+
       <div className={`${styles.content} ${isOpen ? styles.shrink : ""}`}>
         <h1>Welcome to My Website</h1>
         <p>This is the homepage content.</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <h1>Welcome to My Website</h1>
-      <p>This is the homepage content.</p>
     </div>
   );
 };
