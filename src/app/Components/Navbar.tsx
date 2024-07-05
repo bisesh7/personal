b2ea2559f49b7aecbb5e286b2a1182059d98ce12
bisesh shakya <bisesh.shakya@gmail.com>
2024-07-05 11:50:30 -0700

    Use classnames and fix the icons

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,97 +1,141 @@
-// components/Navbar.tsx
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "../styles/Navbar.module.scss";
-import {
-  BiHome,
-  BiUser,
-  BiBook,
-  BiBriefcase,
-  BiLogOut,
-  BiMailSend,
-  BiMenu,
-  BiX,
-} from "react-icons/bi";
+import classnames from "classnames";
+import { MdPerson, MdMail, MdMenu, MdClose, MdArticle } from "react-icons/md";
 import {
   AiFillGithub,
   AiFillLinkedin,
   AiOutlineInstagram,
   AiOutlineTwitter,
 } from "react-icons/ai";
+import { FaBriefcase, FaIdCard } from "react-icons/fa";
+import { IoIosHome } from "react-icons/io";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    console.log(pathname);
+  }, []);
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <div className={`${styles.container} ${isOpen ? styles.open : ""}`}>
+    <div className={classnames(styles.container, { [styles.open]: isOpen })}>
       <div
-        className={`${styles.hamburger} ${
-          isOpen ? styles.open : ""
-        } text-white`}
+        className={classnames(styles.hamburger, {
+          [styles.open]: isOpen,
+          "text-white": true,
+        })}
         onClick={toggleNavbar}
       >
-        {isOpen ? (
-          <BiX className="bi" size="3em" />
-        ) : (
-          <BiMenu className="bi" size="3em" />
-        )}
+        {isOpen ? <MdClose size="3em" /> : <MdMenu size="3em" />}
       </div>
-      <div className={`${styles.navbarContainer} ${isOpen ? styles.open : ""}`}>
+      <div
+        className={classnames(styles.navbarContainer, {
+          [styles.open]: isOpen,
+        })}
+      >
         <nav className={styles.navbar}>
-          <a href="#" className="me-4">
-            <div className="d-flex justify-content-center">
-              <BiHome className="navbar-icon" size={"1.4em"} />
+          <Link href="/" passHref>
+            <div
+              className={classnames(styles.navbarSpace, {
+                [styles.active]: pathname === "/",
+                [styles.link]: true,
+              })}
+            >
+              <div className="d-flex justify-content-center">
+                <IoIosHome className="navbar-icon" size={"1.4em"} />
+              </div>
+              <strong>
+                <small>HOME</small>
+              </strong>
             </div>
-            <strong>
-              <small>HOME</small>
-            </strong>
-          </a>
-          <a href="#" className="me-4">
-            <div className="d-flex justify-content-center">
-              <BiUser className="navbar-icon" size={"1.4em"} />
+          </Link>
+          <Link href="/about" passHref>
+            <div
+              className={classnames(styles.navbarSpace, {
+                [styles.active]: pathname === "/about",
+                [styles.link]: true,
+              })}
+            >
+              <div className="d-flex justify-content-center">
+                <MdPerson className="navbar-icon" size={"1.4em"} />
+              </div>
+              <strong>
+                <small>ABOUT</small>
+              </strong>
             </div>
-            <strong>
-              <small>ABOUT</small>
-            </strong>
-          </a>
-          <a href="#" className="me-4">
-            <div className="d-flex justify-content-center">
-              <BiBook className="navbar-icon" size={"1.4em"} />
+          </Link>
+          <Link href="/resume" passHref>
+            <div
+              className={classnames(styles.navbarSpace, {
+                [styles.active]: pathname === "/resume",
+                [styles.link]: true,
+              })}
+            >
+              <div className="d-flex justify-content-center">
+                <FaIdCard className="navbar-icon" size={"1.4em"} />
+              </div>
+              <strong>
+                <small>RESUME</small>
+              </strong>
             </div>
-            <strong>
-              <small>RESUME</small>
-            </strong>
-          </a>
-          <a href="#" className="me-4">
-            <div className="d-flex justify-content-center">
-              <BiBriefcase className="navbar-icon" size={"1.4em"} />
+          </Link>
+          <Link href="/portfolio" passHref>
+            <div
+              className={classnames(styles.navbarSpace, {
+                [styles.active]: pathname === "/portfolio",
+                [styles.link]: true,
+              })}
+            >
+              <div className="d-flex justify-content-center">
+                <FaBriefcase className="navbar-icon" size={"1.36em"} />
+              </div>
+              <strong>
+                <small>PORTFOLIO</small>
+              </strong>
             </div>
-            <strong>
-              <small>PORTFOLIO</small>
-            </strong>
-          </a>
-          <a href="#" className="me-4">
-            <div className="d-flex justify-content-center">
-              <BiLogOut className="navbar-icon" size={"1.4em"} />
+          </Link>
+          <Link href="/blog" passHref>
+            <div
+              className={classnames(styles.navbarSpace, {
+                [styles.active]: pathname === "/blog",
+                [styles.link]: true,
+              })}
+            >
+              <div className="d-flex justify-content-center">
+                <MdArticle className="navbar-icon" size={"1.4em"} />
+              </div>
+              <strong>
+                <small>BLOG</small>
+              </strong>
             </div>
-            <strong>
-              <small>BLOG</small>
-            </strong>
-          </a>
-          <a href="#" className="me-4">
-            <div className="d-flex justify-content-center">
-              <BiMailSend className="navbar-icon" size={"1.4em"} />
+          </Link>
+          <Link href="/contact" passHref>
+            <div
+              className={classnames(styles.navbarSpace, {
+                [styles.active]: pathname === "/contact",
+                [styles.link]: true,
+              })}
+            >
+              <div className="d-flex justify-content-center">
+                <MdMail className="navbar-icon" size={"1.4em"} />
+              </div>
+              <strong>
+                <small>CONTACT</small>
+              </strong>
             </div>
-            <strong>
-              <small>CONTACT</small>
-            </strong>
-          </a>
+          </Link>
         </nav>
-        <div className={`${styles.socialIcons} ${isOpen ? styles.open : ""}`}>
+        <div
+          className={classnames(styles.socialIcons, { [styles.open]: isOpen })}
+        >
           <a href="#">
             <AiFillGithub className="social-icon me-4" size={"1.4em"} />
           </a>
@@ -110,10 +154,10 @@ const Navbar: React.FC = () => {
         </div>
       </div>
 
-      <div className={`${styles.boxTop}`}></div>
-      <div className={`${styles.boxBottom}`}></div>
+      <div className={styles.boxTop}></div>
+      <div className={styles.boxBottom}></div>
 
-      <div className={`${styles.content} ${isOpen ? styles.shrink : ""}`}>
+      <div className={classnames(styles.content, { [styles.shrink]: isOpen })}>
         <h1>Welcome to My Website</h1>
         <p>This is the homepage content.</p>
       </div>
